bbc10b9c81d4251fd9015fef6f8ef751280129d5
JpBaraky <jp.baraky@gmail.com>
2021-11-02 15:18:51 -0300

    Game Assets script
    -Made a script encapsulates all of my store items as sprites in a object, this way I can access then more easily.
    - 2 of each clothing and 1 for a "naked" sprite

--- Assets/Scripts/gameAssets.cs
+++ Assets/Scripts/gameAssets.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class gameAssets : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private static gameAssets i;
+    public static gameAssets instance{
+        get{
+            if (i == null)
+            {i = (Instantiate(Resources.Load("gameAssets")) as GameObject).GetComponent<gameAssets>();} //If the internal value is null, as in the first time it runs, instatiate the assets as game objects
+        return i;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+//## All sprites inside a Object
+    public Sprite ShirtNone;
+    public Sprite Shirt_1;
+    public Sprite Shirt_2;
+    public Sprite BootsNone;
+    public Sprite Boots_1;
+    public Sprite Boots_2;
+    public Sprite HatNone;
+    public Sprite Hat_1;
+    public Sprite Hat_2;
+//##
 }
