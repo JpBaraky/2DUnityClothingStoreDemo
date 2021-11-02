34280fc4aa9d1e24483dcc1aebf4da5e12e9460f
JpBaraky <jp.baraky@gmail.com>
2021-11-02 19:08:23 -0300

    Button Update
    -Now the button is clickable and responds what item the player has gotten.

--- Assets/Scripts/shopController.cs
+++ Assets/Scripts/shopController.cs
@@ -9,11 +9,15 @@ public class shopController : MonoBehaviour
   
     private Transform containerStore;
     private Transform storeButtonTemplate;
+    private Image containerImage;
+    private Button buyButton;
     void Awake()
     {
         containerStore = transform.Find("ContainerStore"); //Finds and puts the Container Store of the UI in a variable
+        containerImage = containerStore.GetComponent<Image>();
         storeButtonTemplate = containerStore.Find("StoreButtonTemplate");//Finds inside the container and puts the template for the button in a variable.
-        storeButtonTemplate.gameObject.SetActive(false); //Starting with the template hidden
+        storeButtonTemplate.gameObject.SetActive(false); //Starting with the template hidden, he stays hidden because he is there just to be cloned
+        containerImage.enabled = false; //The container starts hidden too, but turn on when is needed
 
     }
     private void Start(){
@@ -28,7 +32,7 @@ public class shopController : MonoBehaviour
         shopItemTransform.gameObject.SetActive(true);
         RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
 
-        float shopItemHeight = 250f;
+        float shopItemHeight = 250f; // Distance between the buttons of the shop
         shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
 
         
@@ -36,6 +40,20 @@ public class shopController : MonoBehaviour
 
         shopItemTransform.Find("Store Item").GetComponent<Image>().sprite = itemSprite;
 
+        buyButton = shopItemTransform.GetComponent<Button>();
+        buyButton.onClick.AddListener(delegate {TryBuyItem(itemType); });
+
+            
+            
+       }
+         public void TryBuyItem(shopItems.ItemTypes itemType){
+             Debug.Log(itemType);
+
+        } 
+
+        
+         //Geting the click on the shop button 
+
                   
        }
-}
+
